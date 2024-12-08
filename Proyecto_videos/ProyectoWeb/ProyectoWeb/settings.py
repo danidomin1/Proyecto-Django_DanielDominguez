e992083cc5cd698904b25d65bea23ee1c5e08f62
danidomin1 <156322933+danidomin1@users.noreply.github.com>
2024-12-08 18:31:56 +0100

    Update1 settings.py

--- Proyecto_videos/ProyectoWeb/ProyectoWeb/settings.py
+++ Proyecto_videos/ProyectoWeb/ProyectoWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r2qy=tae+c2usc*pdr^w67rf9c(xr)d&thiiwc$2is%y*n_a*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.213.20.230', 'localhost']
 
 
 # Application definition
